1aa192e9690d62c5fa5bcbf2b705b06510202b7a
Muhkeun <55132361+Muhkeun@users.noreply.github.com>
2020-07-16 17:41:26 +0900

    Create AutoBoxing.java

---
+++ java0716/AutoBoxing.java
@@ -0,0 +1,17 @@
+class AutoBoxing.java
+{
+	public static void main(String[] args) 
+	{
+	//문자형 상수 A 선언
+	 char a = 'A';
+
+	//정수형 변수에 변수a 대입
+	 int b = a;
+	
+	//결과확인
+	 System.out.println(a);
+	 System.out.println(b);
+
+	//문자형 A의 아스키 코드값(정수)이 변수 b에 대입되는 것을 알 수 있다.
+	}
+}
