9231020b3004d1a972084ce71e553a325f691fb2
Muhkeun <55132361+Muhkeun@users.noreply.github.com>
2020-07-16 17:19:59 +0900

    Update DataType.java

--- java0716/DataType.java
+++ java0716/DataType.java
@@ -4,20 +4,20 @@ class DataType
 	{	
 		//정수
 		int a = 200;
+		//변수 출력 인터프리터 방식 (선언 후 바로 바로 출력. 오류 찾기 쉬워짐)
+		System.out.println(a);
 		
 		//논리형: true(1) / false(0)
 		boolean b = false;
+		System.out.println(b);
 		
 		//문자형
 		char c = 'A';
-
+		System.out.println(c);
+		
 		//실수형
 		double d = 0; 
-
-		//변수 출력
-		System.out.println(a);
-		System.out.println(b);
-		System.out.println(c);
 		System.out.println(d);
+		
 	}
 }
