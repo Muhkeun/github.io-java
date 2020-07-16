57836f9242e015c9ea25ba5126fc32f82702fd99
Muhkeun <55132361+Muhkeun@users.noreply.github.com>
2020-07-16 17:13:23 +0900

    Create Veriable.java

---
+++ java0716/Veriable.java
@@ -0,0 +1,24 @@
+class Variable
+{
+    //변수 - 하나의 값 메모리
+	//선언(메모리 생성) => 대입 순으로 진행
+	
+	public static void main(String[] args) {
+	
+		//변수 a 선언
+		int a;
+		//변수에 상수 대입
+		a = 7;
+		
+		//변수 b 선언과 대입을 한번에
+		int b = 200;
+		
+		//변수 c, d를 선언
+		int c,d;
+		//변수 c, d에 상수 대입
+		c = 50;
+		d = 20000;
+		
+
+	}
+}
