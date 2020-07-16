45468c7179596001647966918af589ba390674c1
Muhkeun <55132361+Muhkeun@users.noreply.github.com>
2020-07-16 17:14:08 +0900

    Create DataType.java

---
+++ java0716/DataType.java
@@ -0,0 +1,18 @@
+class DataType 
+{
+	public static void main(String[] args) 
+	{	
+		//정수
+		int a = 200;
+		
+		//논리형: true(1) / false(0)
+		boolean b = false;
+		
+		//문자형
+		char c = 'A';
+
+		//실수형
+		double d = 0; 
+
+	}
+}
