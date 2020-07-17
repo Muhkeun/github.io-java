9e45e5a015a5bf0b852fe8c7c88244cab878ab39
Muhkeun <55132361+Muhkeun@users.noreply.github.com>
2020-07-17 14:10:46 +0900

    Create Operator04.java

---
+++ java0717/Operator04.java
@@ -0,0 +1,13 @@
+class Operator04{
+	public static void main(String[] args){
+		//대입 연산자 
+		// '=' : 연산자를 중심으로 오른쪽 변수값을 왼쪽 변수에 대입
+		// '+=': 왼쪽 변수에 더하면서 대입
+		int a = 10;
+		
+		//결과 20,20
+		//a += 10, a에 10을 더하고 변수에 대입
+		System.out.println(a += 10);
+		System.out.println(a);
+	}
+}
