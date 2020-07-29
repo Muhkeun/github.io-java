cf0ba51e741f7accb94e080366d316b8427f8038
Muhkeun <55132361+Muhkeun@users.noreply.github.com>
2020-07-29 10:25:25 +0900

    Update abstractEx.java

--- java0729/abstractEx.java
+++ java0729/abstractEx.java
@@ -22,6 +22,7 @@ abstract class Test{
 }
 class A extends Test{
 	//추상 메서드는 자식 클래스에서 오버라이딩 될 수 있다.
+	//클래식 메서드는 추상 메서드가 될 수 없다.
 	void add(){
 		System.out.println("상속된 클래스에서 오버라이딩 가능");
 	}
@@ -31,6 +32,44 @@ class A extends Test{
 	void add3(){}
 	void add4(){}
 
+}
+//인터페이스(Interface) 
+//다중 구현과 메시지 사용의 일관성을 위해 나온 개념
+//인터페이스에 정의된 모든 메소드는 추상 메소드이므로 상속 시
+//전부 오버라이딩해야 한다.
+interface Test1{
+	//추상 메서드만 사용가능
+	//static final 상수만 사용가능
+	public static final int NUM = 10;
+	public static final String Name = "LG";
+
+	public abstract void add();
+	public abstract int sum();
+	String getName();
+}
+interface Test2{
+	
+}
+
+
+//클래스 생성 시 인터페이스를 구현하기 위해서는 implements 예약어를 사용
+//인터페이스를 여러 개 상속 받을 수 있다. 
+class B extends Test implements Test1, Test2 {
+	
+	//마찬가지로 모든 메서드를 오버라이딩 해줘야 한다.	
+	public void add(){}
+	void add2(){}
+	void add3(){}
+	void add4(){}
+
+	public int sum(){
+		return 1;
+	}
+	public String getName(){
+		
+		return "구현부";
+	}
+	
 }
 
 class abstractEx{
@@ -43,5 +82,6 @@ class abstractEx{
 
 		A a = new A();
 		a.add();
+
 	}
 }
