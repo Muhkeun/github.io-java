02a53811320e69d77a0f52ed1588ac17764dbffe
Muhkeun <muhkeun@naver.com>
2020-08-07 11:29:11 +0900

    Review0807

---
+++ java0807/src/Review0807/TCPServer.java
@@ -0,0 +1,37 @@
+package Review0807;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Date;
+
+public class TCPServer {
+    public static void main(String[] args) {
+        try {
+            //소켓 생성 => ServerSocket(PortNum);
+            ServerSocket ss = new ServerSocket(포트번);
+            while (true){
+                //연결
+                //user : 접속한 사용자 정보
+                Socket user = ss.accept();
+                OutputStream os =user.getOutputStream();
+                ObjectOutputStream oos = new ObjectOutputStream(os);
+                //전송
+                oos.writeObject(new Date());
+                FileOutputStream log = new FileOutputStream("파일경로" );
+                Date date = new Date();
+                String str = date.toString() + " " +  user.toString();
+                byte [] bytes = str.getBytes();
+                log.write(bytes);
+                oos.flush();
+                //연결 종료
+                oos.close();
+                user.close();
+
+
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
---
+++ java0807/src/Review0807/TCPUser.java
@@ -0,0 +1,23 @@
+package Review0807;
+
+
+import java.io.InputStream;
+import java.io.ObjectInputStream;
+import java.net.Socket;
+import java.util.Date;
+
+public class TCPUser {
+    public static void main(String[] args) {
+        try{
+            Socket user = new Socket("ip주소", 포트번호);
+            InputStream is = user.getInputStream();
+            ObjectInputStream ois = new ObjectInputStream(is);
+            Date day = (Date)ois.readObject();
+            System.out.println("시간: " + day);
+            ois.close();
+            user.close();
+        }catch (Exception e){
+            System.out.println("포트번호가 잘못 입력되었습니다.");
+        }
+    }
+}
